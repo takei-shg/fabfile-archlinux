78fb30d7e02695899ad28b06eabf52faca622eba
takei.shg <takei.shg@gmail.com>
2013-11-30 15:00:41 +0900

    add pacman -Syu and yaourt installation
    base.package() fails without yaourt.
    base.package() also fails with file confliction.
    So added -Syu version for base.package() and,
    added yaourt installation.

--- README.md
+++ README.md
@@ -3,14 +3,23 @@
 ## task list
 
 ```sh
+cd fabfile
 fab --list
 ```
 
 ## Usage
 
-in Vagrant,
+First, `vagrant up` and check its private_network IP.
 
 ```sh
-fab -H 127.0.0.1 -u vagrant --port 2222 base.all ruby.base
+fab -H 127.0.0.1 -u vagrant --port 2222 base.all ruby.base haskell.all
+# password: vagrant
+```
+
+In case of failure to install Arch base-devel package,
+
+```sh
+# run the pacman with -Syu
+fab -H 127.0.0.1 -u vagrant --port 2222 base.all_withSysUpgrade ruby.base haskell.all
 # password: vagrant
 ```
--- fabfile/software/base.py
+++ fabfile/software/base.py
@@ -1,9 +1,44 @@
-from fabric.api import task, sudo, run
+from fabric.api import task, sudo, run, cd, settings
+
+@task
+def package_query():
+    with settings(warn_only=True):
+      is_package_query_exist = sudo('pacman -Qs package-query')
+    if is_package_query_exist.failed:
+      sudo('pacman -Sy --noconfirm yajl')
+      with cd('/usr/local/src'):
+        sudo('wget https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz')
+        sudo('tar xvzf package-query.tar.gz')
+      with cd('/usr/local/src/package-query'):
+        sudo('makepkg --asroot ; echo $?')
+        sudo('pacman -U --noconfirm package-query-1.2-2-x86_64.pkg.tar.xz')
+
+@task
+def yaourt():
+    with settings(warn_only=True):
+      is_yaourt_exist = sudo('pacman -Qs yaourt')
+    if is_yaourt_exist.failed:
+      package_query()
+      with cd('/usr/local/src'):
+        sudo('wget https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz')
+        sudo('tar xvzf yaourt.tar.gz')
+      with cd('/usr/local/src/yaourt'):
+        sudo('makepkg --asroot ; echo $?')
+        sudo('pacman -U --noconfirm yaourt-1.3-1-any.pkg.tar.xz')
 
 @task
 def package():
     sudo('pacman -Sy --noconfirm base-devel tmux vim git tig zsh curl wget '
         'sqlite zip unzip')
+
+# if package() fails, update the system
+@task
+def packageSyu():
+    sudo('pacman -Syu --noconfirm base-devel tmux vim git tig zsh curl wget '
+        'sqlite zip unzip')
+
+@task
+def nkf():
     sudo('yaourt -S --noconfirm nkf')
 
 @task
@@ -18,6 +53,19 @@ def all():
     '''
 # base.all
 package()
+yaourt()
+nkf()
 dotfiles()
     '''
     exec(all.__doc__)
+
+@task
+def all_withSysUpgrade():
+    '''
+# base.all_withSysUpgrade
+packageSyu()
+yaourt()
+nkf()
+dotfiles()
+    '''
+    exec(all_withSysUpgrade.__doc__)
